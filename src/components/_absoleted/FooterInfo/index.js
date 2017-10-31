8f4cb09078f6ea2b4088b6e3665b619f84679904
Bryt Li <bryt@oriente.com>
2017-10-31 11:39:16 +0800

    [deploy]

--- src/components/_absoleted/FooterInfo/index.js
+++ src/components/_absoleted/FooterInfo/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import styles from './index.less';
+import React from 'react'
+import styles from './index.less'
 
-const pkg = require('../../../package.json');
-const date = new Date();
+const pkg = require('../../../package.json')
+
+const date = new Date()
 
 function FooterInfo() {
-  return (
-    <div className={styles.normal}>
-	    <div><span>活力火山网上商城</span>©<span>{date.getFullYear()}</span></div>
-	    <div><span>版本：{pkg.version}</span></div>
-    </div>
-  );
+	return (
+	<div className={styles.normal}>
+		<div><span>活力火山网上商城</span>©<span>{date.getFullYear()}</span></div>
+		<div><span>版本：{pkg.version}</span></div>
+	</div>
+	)
 }
 
-export default FooterInfo;
+export default FooterInfo
